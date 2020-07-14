1ddb2dd3520d2a13eaf0d7eee19b10e183c2dbd3
docker <docker@mooom.top>
2020-07-14 11:54:37 +0800

    up 1.18.5

---
+++ up_k8s.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#获取images列表
+#基础镜像
+im1=`kubeadm config images list`
+#ui镜像
+im2=`curl -s https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.3/aio/deploy/recommended.yaml|grep image: |awk '{print $2}'`
+#网络镜像
+im3=`curl -s https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml |grep image|grep amd64|uniq|awk '{print $2}'`
+im4=`curl -s https://docs.projectcalico.org/manifests/calico.yaml |grep image|uniq|awk '{print $2}'`
+#镜像汇总
+ki=(`echo $im1 $im2 $im3 $im4`)
+#创建目录
+for i in ${ki[@]}; do mkdir `echo ${i}|awk -F / '{print $2}'|awk -F : '{print $1}'`; done
+#创建dockerfile
+for i in ${ki[@]}; do echo -e "FROM ${i}\nMAINTAINER dongs365" > `echo ${i}|awk -F / '{print $2}'|awk -F : '{print $1}'`/Dockerfile; done
