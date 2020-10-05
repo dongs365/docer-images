c8932a9e542438beda809bcdb526c1af1b747053
docker <docker@mooom.top>
2020-10-05 17:52:51 +0800

    :sparkles:Mon Oct  5 17:52:51 CST 2020

--- up_k8s_image.sh
+++ up_k8s_image.sh
@@ -10,7 +10,7 @@ im4=`curl -s https://docs.projectcalico.org/manifests/calico.yaml |grep image|un
 #镜像汇总
 ki=(`echo $im1 $im2 $im3 $im4`)
 #创建目录
-#for i in ${ki[@]}; do mkdir `echo ${i}|awk -F / '{print $2}'|awk -F : '{print $1}'`; done
+for i in ${ki[@]}; do mkdir `echo ${i}|awk -F / '{print $2}'|awk -F : '{print $1}'`; done
 #创建dockerfile
 for i in ${ki[@]}; do echo -e "FROM ${i}\nMAINTAINER dongs365" > `echo ${i}|awk -F / '{print $2}'|awk -F : '{print $1}'`/Dockerfile; done
 mv coreos flannel
