63c6a32783d830cd18569c30546b4162aaabf471
dongs365 <you@example.com>
2023-04-06 16:10:12 +0800

    :whale: Thu Apr  6 16:10:12 CST 2023

--- up_k8s_image.sh
+++ up_k8s_image.sh
@@ -3,7 +3,7 @@
 #基础镜像
 im1=`kubeadm config images list`
 #ui镜像
-im2=`curl -s https://raw.githubusercontent.com/kubernetes/dashboard/v2.2.0/aio/deploy/recommended.yaml|grep image: |awk '{print $2}'`
+im2=`curl -s https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml|grep image: |awk '{print $2}'`
 #网络镜像
 im3=`curl -s https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml |grep image|uniq|awk '{print $2}'`
 im4=`curl -s https://docs.projectcalico.org/manifests/calico.yaml |grep image|uniq|awk '{print $2}'`
